64a73ecf8d74beddd2bf8a523e5e0cf4cb3c7cd7
MohammedImdadS <imdadsmd@gmail.com>
2023-02-13 09:57:27 +0530

    update

--- StateManagement/bloc_practice/lib/main.dart
+++ StateManagement/bloc_practice/lib/main.dart
@@ -42,9 +42,9 @@ extension UrlString on PersonUrl {
   String get urlString {
     switch (this) {
       case PersonUrl.persons1:
-        return 'http://127.0.0.1:5500/api/persons1.json';
+        return 'https://raw.githubusercontent.com/MohammedImdadS/FlutterPractice/main/StateManagement/bloc_practice/api/persons1.json';
       case PersonUrl.persons2:
-        return 'http://127.0.0.1:5500/api/persons2.json';
+        return 'https://raw.githubusercontent.com/MohammedImdadS/FlutterPractice/main/StateManagement/bloc_practice/api/persons2.json';
     }
   }
 }
